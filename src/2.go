5c87e107afdbb7e40023ea9686d706fbd2407645
Artemiy Rodionov <wertins71@gmail.com>
2020-12-05 16:26:41 +0300

    Solution 2

---
+++ src/2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "strconv"
+    "log"
+    "strings"
+)
+
+func fail(err error) {
+    if err != nil { log.Fatal(err) }
+}
+
+func Fn() {
+    scanner := bufio.NewScanner(os.Stdin)
+    validNumber := 0
+    validNumber2 := 0
+    for scanner.Scan() {
+        policy := strings.Split(scanner.Text(), " ")
+        if len(policy) > 3 {
+            log.Fatal("Wrong input", policy)
+        }
+
+        policyRange := strings.Split(policy[0], "-")
+        from, err := strconv.Atoi(policyRange[0])
+        fail(err)
+        to, err := strconv.Atoi(policyRange[1])
+        fail(err)
+
+        char := rune(policy[1][:1][0])
+        password := policy[2]
+
+        count := 0
+        isValid := false
+        for i, pchar := range password {
+            if char == pchar {
+                count++
+            }
+            idx := i + 1
+            if (idx == from || idx == to) && pchar == char {
+                isValid = ! isValid
+            }
+        }
+
+        if from <= count && to >= count {
+            validNumber++
+        }
+        if isValid {
+            validNumber2++
+        }
+    }
+    log.Println(validNumber)
+    log.Println(validNumber2)
+}
+
