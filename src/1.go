3a98b36fa4c234b7a4dcff78ce1e04fdaf9eaad6
Artemiy Rodionov <wertins71@gmail.com>
2020-12-05 14:17:39 +0300

    Solution 1.1

--- src/1.go
+++ src/1.go
@@ -3,12 +3,35 @@ package main
 import (
     "os"
     "bufio"
+    "strconv"
+    "log"
 )
+
+type Exist struct {}
+const ExpectedSum = 2020
 
 func Fn() {
     scanner := bufio.NewScanner(os.Stdin)
+    var numbers []int
     for scanner.Scan() {
-        println(scanner.Text())
+        val, err := strconv.Atoi(scanner.Text())
+        if err != nil {
+            log.Fatal(err)
+        }
+        numbers = append(numbers, val)
+    }
+
+    result := make(map[int]Exist)
+    for i, first := range numbers {
+        for j, second := range numbers {
+            if i == j { continue }
+            if first + second == ExpectedSum {
+                result[first * second] = Exist{}
+            }
+        }
+    }
+    for number := range result {
+        log.Println(number)
     }
 }
 
