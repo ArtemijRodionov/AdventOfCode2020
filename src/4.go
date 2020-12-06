f6f90e71166a923d9b515593a44dedc1d02e9e3c
Artemiy Rodionov <wertins71@gmail.com>
2020-12-06 16:50:13 +0300

    Solution 4.1

--- src/4.go
+++ src/4.go
@@ -4,67 +4,67 @@ import (
 	"bufio"
 	"log"
 	"os"
-    "strings"
+	"strings"
 )
 
 type Empty struct{}
 type Set map[string]Empty
 
 func NewSet(xs []string) Set {
-    var e Empty
-    s := make(Set)
-    for _, x := range xs {
-        s[x] = e
-    }
-    return s
+	var e Empty
+	s := make(Set)
+	for _, x := range xs {
+		s[x] = e
+	}
+	return s
 }
 
 func (s1 Set) isSubset(s2 Set) bool {
-    for k, _ := range s1 {
-        if _, ok := s2[k]; !ok {
-            return false
-        }
-    }
-    return true
+	for k, _ := range s1 {
+		if _, ok := s2[k]; !ok {
+			return false
+		}
+	}
+	return true
 }
 
 func Fn() {
 	scanner := bufio.NewScanner(os.Stdin)
-    spec := NewSet([]string{
-        "byr",
-        "iyr",
-        "eyr",
-        "hgt",
-        "hcl",
-        "ecl",
-        "pid",
-    })
-    fields := make([]string, 0)
-    validCount := 0
-    nextPassport := func() {
-        if spec.isSubset(NewSet(fields)) {
-            validCount++
-        }
-        fields = fields[:0]
-    }
-
-	for {
-        if !scanner.Scan() {
-            nextPassport()
-            break
-        }
-
-		rawFields := strings.Fields(scanner.Text())
-        if len(rawFields) == 0 {
-            nextPassport()
-            continue
-        }
-
-        for _, rawField := range rawFields {
-            field := strings.Split(rawField, ":")
-            fields = append(fields, field[0])
-        }
+	spec := NewSet([]string{
+		"byr",
+		"iyr",
+		"eyr",
+		"hgt",
+		"hcl",
+		"ecl",
+		"pid",
+	})
+	fields := make([]string, 0)
+	validCount := 0
+	nextPassport := func() {
+		if spec.isSubset(NewSet(fields)) {
+			validCount++
+		}
+		fields = fields[:0]
 	}
 
-    log.Println(validCount)
+	for {
+		if !scanner.Scan() {
+			nextPassport()
+			break
+		}
+
+		rawFields := strings.Fields(scanner.Text())
+		if len(rawFields) == 0 {
+			nextPassport()
+			continue
+		}
+
+		for _, rawField := range rawFields {
+			field := strings.Split(rawField, ":")
+			fields = append(fields, field[0])
+		}
+	}
+
+	log.Println(validCount)
 }
