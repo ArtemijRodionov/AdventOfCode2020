bb81bd67dccb6cc49f8b118f4ab9912743b907d6
Artemiy Rodionov <wertins71@gmail.com>
2020-12-06 14:01:56 +0300

    Fmt code

--- run.go
+++ run.go
@@ -1,54 +1,53 @@
 package main
 
 import (
-    "plugin"
-    "os"
-    "os/exec"
-    "io/ioutil"
-    "path/filepath"
-    "log"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"plugin"
 )
 
 func buildPlugin(taskNumber string) {
-    taskPath := filepath.Join("src/", taskNumber + ".go")
-    cmd := exec.Command("go", "build", "-buildmode=plugin", taskPath)
-    stderr, err := cmd.StderrPipe()
-    if err != nil {
-        log.Fatal(err)
-    }
-    if err := cmd.Start(); err != nil {
-        log.Fatal(err)
-    }
-    msg, err := ioutil.ReadAll(stderr)
-    if err != nil || string(msg) != "" {
-        log.Fatal(err, string(msg))
-    }
+	taskPath := filepath.Join("src/", taskNumber+".go")
+	cmd := exec.Command("go", "build", "-buildmode=plugin", taskPath)
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	msg, err := ioutil.ReadAll(stderr)
+	if err != nil || string(msg) != "" {
+		log.Fatal(err, string(msg))
+	}
 	if err := cmd.Wait(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func runPlugin(taskNumber string) {
-    so := taskNumber + ".so"
-    plug, err := plugin.Open(so)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fn, err := plug.Lookup("Fn")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fn.(func())()
+	so := taskNumber + ".so"
+	plug, err := plugin.Open(so)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fn, err := plug.Lookup("Fn")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fn.(func())()
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        log.Fatal("Provide task number as the first argument")
-    }
-    taskNumber := os.Args[1]
-    log.Println("Build task")
-    buildPlugin(taskNumber)
-    log.Println("Run task")
-    runPlugin(taskNumber)
+	if len(os.Args) < 2 {
+		log.Fatal("Provide task number as the first argument")
+	}
+	taskNumber := os.Args[1]
+	log.Println("Build task")
+	buildPlugin(taskNumber)
+	log.Println("Run task")
+	runPlugin(taskNumber)
 }
-
--- src/1.go
+++ src/1.go
@@ -1,48 +1,52 @@
 package main
 
 import (
-    "os"
-    "bufio"
-    "strconv"
-    "log"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
 )
 
-type Exist struct {}
+type Exist struct{}
+
 const ExpectedSum = 2020
 
 func Fn() {
-    scanner := bufio.NewScanner(os.Stdin)
-    var numbers []int
-    for scanner.Scan() {
-        val, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-            log.Fatal(err)
-        }
-        numbers = append(numbers, val)
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	var numbers []int
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, val)
+	}
 
-    firstResult := make(map[int]Exist)
-    secondResult := make(map[int]Exist)
-    for i, first := range numbers {
-        for j, second := range numbers {
-            if i == j { continue }
-            if first + second == ExpectedSum {
-                firstResult[first * second] = Exist{}
-            }
+	firstResult := make(map[int]Exist)
+	secondResult := make(map[int]Exist)
+	for i, first := range numbers {
+		for j, second := range numbers {
+			if i == j {
+				continue
+			}
+			if first+second == ExpectedSum {
+				firstResult[first*second] = Exist{}
+			}
 
-            for y, third := range numbers {
-                if j == y || i == y { continue }
-                if first + second + third == ExpectedSum {
-                    secondResult[first * second * third] = Exist{}
-                }
-            }
-        }
-    }
-    for number := range firstResult {
-        log.Println("First", number)
-    }
-    for number := range secondResult {
-        log.Println("Second", number)
-    }
+			for y, third := range numbers {
+				if j == y || i == y {
+					continue
+				}
+				if first+second+third == ExpectedSum {
+					secondResult[first*second*third] = Exist{}
+				}
+			}
+		}
+	}
+	for number := range firstResult {
+		log.Println("First", number)
+	}
+	for number := range secondResult {
+		log.Println("Second", number)
+	}
 }
-
