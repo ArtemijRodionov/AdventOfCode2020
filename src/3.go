b7443f27708e7f0e6c428cc73770a57f2faa1baa
Artemiy Rodionov <wertins71@gmail.com>
2020-12-06 14:35:16 +0300

    Solution 3.2

--- src/3.go
+++ src/3.go
@@ -6,18 +6,36 @@ import (
 	"os"
 )
 
+type Slope struct { X, Y int }
+
 func Fn() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var grid []string
 	for scanner.Scan() {
 		grid = append(grid, scanner.Text())
 	}
-	treeCount := 0
-	for row := 1; row < len(grid); row++ {
-		col := (row * 3) % len(grid[row])
-		if rune(grid[row][col]) == '#' {
-			treeCount++
-		}
-	}
-	log.Println(treeCount)
+
+    slopes := []Slope{
+        Slope{1, 1},
+        Slope{3, 1},
+        Slope{5, 1},
+        Slope{7, 1},
+        Slope{1, 2},
+    }
+
+    slopeTreeCount := make(map[Slope]int)
+    for _, slope := range slopes {
+        for i, row := 1, slope.Y; row < len(grid); i, row = i+1, row+slope.Y {
+            col := (i * slope.X) % len(grid[row])
+            if rune(grid[row][col]) == '#' {
+                slopeTreeCount[slope]++
+            }
+        }
+    }
+	log.Println(slopeTreeCount)
+    result := 1
+    for _, count := range slopeTreeCount {
+        result *= count
+    }
+    log.Println(result)
 }
