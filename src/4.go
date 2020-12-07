ed0e02f07bfa315a76c513721c424ec74a9cc8e0
Artemiy Rodionov <wertins71@gmail.com>
2020-12-07 10:16:10 +0300

    Solution 4.2

--- src/4.go
+++ src/4.go
@@ -4,8 +4,54 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+type Validator func(string) bool
+
+func inRange(from, to int) Validator {
+	return func(s string) bool {
+		if val, err := strconv.Atoi(s); err == nil {
+			return from <= val && to >= val
+		}
+		return false
+	}
+}
+func hasPattern(exp string) Validator {
+	re := regexp.MustCompile(exp)
+	return func(s string) bool {
+		return re.Match([]byte(s))
+	}
+}
+func const_(x bool) Validator {
+	return func(s string) bool { return x }
+}
+func or(xs ...Validator) Validator {
+	return func(s string) bool {
+		for _, x := range xs {
+			if x(s) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+var validators = map[string]Validator{
+	"byr": inRange(1920, 2002),
+	"iyr": inRange(2010, 2020),
+	"eyr": inRange(2020, 2030),
+	"hgt": or(
+		hasPattern("(1[5-8][0-9]|19[0-3])cm"),
+		hasPattern("(59|6[0-9]|7[0-6])in"),
+	),
+	"hcl": hasPattern("#[0-9a-f]{6}"),
+	"ecl": hasPattern("(amb|blu|brn|gry|grn|hzl|oth)"),
+	"pid": hasPattern("[0-9]{9}"),
+	"cid": const_(true),
+}
 
 type Empty struct{}
 type Set map[string]Empty
@@ -39,13 +85,29 @@ func Fn() {
 		"ecl",
 		"pid",
 	})
-	fields := make([]string, 0)
+
 	validCount := 0
+	validCount2 := 0
+	fieldVals := make(map[string]string, 0)
+
 	nextPassport := func() {
+		isValid := true
+		fields := make([]string, len(fieldVals))
+		for f, v := range fieldVals {
+			fields = append(fields, f)
+			if validator, ok := validators[f]; !(ok && validator(v)) {
+				isValid = false
+			}
+		}
 		if spec.isSubset(NewSet(fields)) {
 			validCount++
+		} else {
+			isValid = false
 		}
-		fields = fields[:0]
+		if isValid {
+			validCount2++
+		}
+		fieldVals = make(map[string]string)
 	}
 
 	for {
@@ -61,10 +123,12 @@ func Fn() {
 		}
 
 		for _, rawField := range rawFields {
-			field := strings.Split(rawField, ":")
-			fields = append(fields, field[0])
+			parsed := strings.Split(rawField, ":")
+			field, val := parsed[0], parsed[1]
+			fieldVals[field] = val
 		}
 	}
 
 	log.Println(validCount)
+	log.Println(validCount2 - 1)
 }
