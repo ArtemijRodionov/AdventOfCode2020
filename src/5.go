abaee78481df2b41a8fdba2023dfcf117ed385ee
Artemiy Rodionov <wertins71@gmail.com>
2020-12-12 19:59:26 +0300

    Solution 5.2

--- src/5.go
+++ src/5.go
@@ -19,20 +19,34 @@ func parse(s string, lo, hi int, loChar, hiChar rune) int {
 		return hi
 	}
 	return lo
-
 }
 
 func Fn() {
 	scanner := bufio.NewScanner(os.Stdin)
 	maxId := 0
+    idPass := make(map[int]struct{})
 	for scanner.Scan() {
 		raw := scanner.Text()
 		row := parse(raw[:7], 0, 127, 'F', 'B')
 		col := parse(raw[7:], 0, 7, 'L', 'R')
 		id := row*8 + col
+        idPass[id] = struct{}{}
 		if maxId < id {
 			maxId = id
 		}
 	}
+    missedId := 0
+    offsets := []int{1, -1}
+    for id := range idPass {
+        for x := range offsets {
+            _, boundOk := idPass[id+2*x]
+            _, centerOk := idPass[id+1*x]
+            if boundOk && !centerOk {
+                missedId = id + 1*x
+                break
+            }
+        }
+    }
 	log.Println(maxId)
+	log.Println(missedId)
 }
