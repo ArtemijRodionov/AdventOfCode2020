10a29da9cf8721648f6021af3f099d213c63328e
Artemiy Rodionov <wertins71@gmail.com>
2020-12-13 15:29:57 +0300

    Fmt code

--- src/6.go
+++ src/6.go
@@ -8,35 +8,37 @@ import (
 
 func Fn() {
 	scanner := bufio.NewScanner(os.Stdin)
-    raw := ""
-    anyCount := 0
-    allCount := 0
-    groupSize := 0
-    yes := make(map[rune]int)
+	raw := ""
+	anyCount := 0
+	allCount := 0
+	groupSize := 0
+	yes := make(map[rune]int)
 	for {
-        isOk := scanner.Scan()
-        if isOk {
-            raw = scanner.Text()
-            for _, r := range raw {
-                yes[r]++
-            }
-        }
+		isOk := scanner.Scan()
+		if isOk {
+			raw = scanner.Text()
+			for _, r := range raw {
+				yes[r]++
+			}
+		}
 
-        if raw == "" || !isOk {
-            for _, count := range yes {
-                if count == groupSize {
-                    allCount++
-                }
-            }
-            anyCount += len(yes)
-            groupSize = 0
-            yes = make(map[rune]int)
-        } else {
-            groupSize++
-        }
+		if raw == "" || !isOk {
+			for _, count := range yes {
+				if count == groupSize {
+					allCount++
+				}
+			}
+			anyCount += len(yes)
+			groupSize = 0
+			yes = make(map[rune]int)
+		} else {
+			groupSize++
+		}
 
-        if !isOk { break }
+		if !isOk {
+			break
+		}
 	}
-    log.Println(anyCount)
-    log.Println(allCount)
+	log.Println(anyCount)
+	log.Println(allCount)
 }
--- src/7.go
+++ src/7.go
@@ -4,74 +4,81 @@ import (
 	"bufio"
 	"log"
 	"os"
-    "strings"
-    "strconv"
+	"strconv"
+	"strings"
 )
+
 type Counter map[string]int
 type Index map[string]Counter
 
 const (
-    RuleSep = "contain"
-    CapSep = ","
-    NumSep = " "
-    ToTrim = ". "
+	RuleSep = "contain"
+	CapSep  = ","
+	NumSep  = " "
+	ToTrim  = ". "
 )
 
 var ToRemove = strings.NewReplacer(
-    "bags", "",
-    "bag", "",
+	"bags", "",
+	"bag", "",
 )
 
 func traverse(idx Index, name string, fn func(string)) {
-    outBags, ok := idx[name]
-    if !ok { return }
-    for bagName := range outBags {
-        fn(bagName)
-        traverse(idx, bagName, fn)
-    }
+	outBags, ok := idx[name]
+	if !ok {
+		return
+	}
+	for bagName := range outBags {
+		fn(bagName)
+		traverse(idx, bagName, fn)
+	}
 }
 
 func countBags(idx Index, name string) int {
-    bags, ok := idx[name]
-    if !ok { return 0 }
-    result := 0
-    for bag, count := range bags {
-        result += count + count * countBags(idx, bag)
-    }
-    return result
+	bags, ok := idx[name]
+	if !ok {
+		return 0
+	}
+	result := 0
+	for bag, count := range bags {
+		result += count + count*countBags(idx, bag)
+	}
+	return result
 }
 
 func Fn() {
 	scanner := bufio.NewScanner(os.Stdin)
-    innerOuter := make(Index)
-    outerInner := make(Index)
-    for scanner.Scan() {
-        line := scanner.Text()
+	innerOuter := make(Index)
+	outerInner := make(Index)
+	for scanner.Scan() {
+		line := scanner.Text()
 
-        ruleCaps := strings.Split(line, RuleSep)
-        caps := strings.Split(strings.TrimSpace(ruleCaps[1]), CapSep)
-        outer := strings.TrimSpace(ToRemove.Replace(ruleCaps[0]))
-        outerInner[outer] = make(Counter)
-        for _, cap_ := range caps {
-            numName := strings.SplitN(strings.Trim(cap_, ToTrim), NumSep, 2)
-            rawCapNum := strings.TrimSpace(numName[0])
-            capNum, err := strconv.Atoi(rawCapNum)
-            if err != nil { capNum = 0 }
-            inner := strings.TrimSpace(ToRemove.Replace(numName[1]))
-            outerInner[outer][inner] = capNum
-            if _, ok := innerOuter[inner]; !ok {
-                innerOuter[inner] = make(Counter)
-            }
-            innerOuter[inner][outer] = capNum
-        }
-    }
+		ruleCaps := strings.Split(line, RuleSep)
+		caps := strings.Split(strings.TrimSpace(ruleCaps[1]), CapSep)
+		outer := strings.TrimSpace(ToRemove.Replace(ruleCaps[0]))
+		outerInner[outer] = make(Counter)
+		for _, cap_ := range caps {
+			numName := strings.SplitN(strings.Trim(cap_, ToTrim), NumSep, 2)
+			rawCapNum := strings.TrimSpace(numName[0])
+			capNum, err := strconv.Atoi(rawCapNum)
+			if err != nil {
+				capNum = 0
+			}
+			inner := strings.TrimSpace(ToRemove.Replace(numName[1]))
+			outerInner[outer][inner] = capNum
+			if _, ok := innerOuter[inner]; !ok {
+				innerOuter[inner] = make(Counter)
+			}
+			innerOuter[inner][outer] = capNum
+		}
+	}
 
-    bag := "shiny gold"
-    outerBags := make(Counter)
-    traverse(innerOuter, bag, func(bagName string) {
-        outerBags[bagName]++
-    })
-    innerCount := countBags(outerInner, bag)
-    log.Println(len(outerBags))
-    log.Println(innerCount)
+	bag := "shiny gold"
+	outerBags := make(Counter)
+	traverse(innerOuter, bag, func(bagName string) {
+		outerBags[bagName]++
+	})
+	innerCount := countBags(outerInner, bag)
+	log.Println(len(outerBags))
+	log.Println(innerCount)
 }
