8c9371f525e3eec3f027bf0ac1792c71d436293e
Artemiy Rodionov <wertins71@gmail.com>
2020-12-13 15:29:27 +0300

    Solution 7.2

--- src/7.go
+++ src/7.go
@@ -31,22 +31,34 @@ func traverse(idx Index, name string, fn func(string)) {
     }
 }
 
+func countBags(idx Index, name string) int {
+    bags, ok := idx[name]
+    if !ok { return 0 }
+    result := 0
+    for bag, count := range bags {
+        result += count + count * countBags(idx, bag)
+    }
+    return result
+}
+
 func Fn() {
 	scanner := bufio.NewScanner(os.Stdin)
     innerOuter := make(Index)
+    outerInner := make(Index)
     for scanner.Scan() {
         line := scanner.Text()
 
         ruleCaps := strings.Split(line, RuleSep)
         caps := strings.Split(strings.TrimSpace(ruleCaps[1]), CapSep)
         outer := strings.TrimSpace(ToRemove.Replace(ruleCaps[0]))
-
+        outerInner[outer] = make(Counter)
         for _, cap_ := range caps {
             numName := strings.SplitN(strings.Trim(cap_, ToTrim), NumSep, 2)
             rawCapNum := strings.TrimSpace(numName[0])
             capNum, err := strconv.Atoi(rawCapNum)
             if err != nil { capNum = 0 }
             inner := strings.TrimSpace(ToRemove.Replace(numName[1]))
+            outerInner[outer][inner] = capNum
             if _, ok := innerOuter[inner]; !ok {
                 innerOuter[inner] = make(Counter)
             }
@@ -59,6 +71,7 @@ func Fn() {
     traverse(innerOuter, bag, func(bagName string) {
         outerBags[bagName]++
     })
-
-    log.Printf("%v", len(outerBags))
+    innerCount := countBags(outerInner, bag)
+    log.Println(len(outerBags))
+    log.Println(innerCount)
 }
